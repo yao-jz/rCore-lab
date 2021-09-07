79a6206bcfb9701b052eddd8d78cc6bf5a1b1015
zhanghx0905 <731931282@qq.com>
2021-09-07 16:27:39 +0800

    remove pipelargetest from usertests

--- user/src/bin/usertests.rs
+++ user/src/bin/usertests.rs
@@ -14,9 +14,9 @@ static TESTS: &[&str] = &[
     "stack_overflow\0",
     "yield\0",
     "pipetest\0",
-    "run_pipe_test\0",
     "filetest_simple\0",
     // 正确实现 sys_get_time 后这些程序才能运行
+    // "pipe_large_test\0",
     // "forktest2\0",
     // "sleep\0",
     // "sleep_simple\0",
